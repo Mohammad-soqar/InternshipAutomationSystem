dac9c391125451a5a4f91102c46f6b65ca6ceec0
Mohammad Soqar <mnsoqar1@gmail.com>
2023-05-08 01:14:43 +0300

    Application Db context

---
+++ InternshipAutomationSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InternshipAutomationSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternshipAutomationSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+    }
+        public DbSet<InternshipJob> InternshipJobs { get; set; }
+    }
+}
