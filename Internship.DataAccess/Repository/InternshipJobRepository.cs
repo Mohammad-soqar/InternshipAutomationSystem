504222dd8e707d21847b3fcc7c2ef9d57197a687
Mohammad Soqar <mnsoqar1@gmail.com>
2023-05-08 20:02:17 +0300

    Repository and UnitOfWork

---
+++ Internship.DataAccess/Repository/InternshipJobRepository.cs
@@ -0,0 +1,45 @@
+﻿using Internship.DataAccess.Data;
+using Internship.DataAccess.Repository.IRepository;
+using Internship.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.DataAccess.Repository
+{
+    public class InternshipJobRepository : Repository<InternshipJob>, IInternshipJobRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public InternshipJobRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+
+        }
+
+
+        public void Update(InternshipJob obj)
+        {
+            var objFromDb = _db.InternshipJobs.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.CompanyName = obj.CompanyName;
+                objFromDb.JobTitle = obj.JobTitle;
+                objFromDb.Location = obj.Location;
+                objFromDb.SkillsReq = obj.SkillsReq;
+                objFromDb.type = obj.type;
+                objFromDb.Site = obj.Site;
+                objFromDb.Description = obj.Description;
+            
+                if (objFromDb.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+
+            }
+        }
+    }
+}
---
+++ Internship.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Internship.DataAccess.Data;
+using Internship.DataAccess.Repository.IRepository;
+using Internship.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            InternshipJobs = new InternshipJobRepository(_db);
+           
+
+        }
+
+        public IInternshipJobRepository InternshipJobs { get; private set; }
+
+       
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
