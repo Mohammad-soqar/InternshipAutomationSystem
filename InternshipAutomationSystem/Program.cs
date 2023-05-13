0118e8829f8b545a527afaae6c78e9ffd73ca81d
Mohammad Soqar <mnsoqar1@gmail.com>
2023-05-13 22:16:07 +0300

    Update Program.cs mapping

--- InternshipAutomationSystem/Program.cs
+++ InternshipAutomationSystem/Program.cs
@@ -3,6 +3,8 @@ using Internship.DataAccess.Repository.IRepository;
 using Internship.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using CodyleOffical.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +15,23 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
  builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+
+
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddRazorPages();
-
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -36,9 +49,9 @@ app.UseRouting();
 app.UseAuthentication();;
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=User}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
