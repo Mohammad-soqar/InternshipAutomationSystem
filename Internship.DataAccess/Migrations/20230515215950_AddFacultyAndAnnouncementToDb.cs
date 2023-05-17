70545b9c8dbb6bb43a5ab0ce7641826fe24414a5
Mohammad Soqar <mnsoqar1@gmail.com>
2023-05-17 14:13:30 +0300

    migration

---
+++ Internship.DataAccess/Migrations/20230515215950_AddFacultyAndAnnouncementToDb.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internship.DataAccess.Migrations
+{
+    public partial class AddFacultyAndAnnouncementToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CoordinatorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Faculties_Coordinators_CoordinatorId",
+                        column: x => x.CoordinatorId,
+                        principalTable: "Coordinators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Announcements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CareerCenterId = table.Column<int>(type: "int", nullable: true),
+                    FacultyId = table.Column<int>(type: "int", nullable: false),
+                    DatePosted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Announcements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Announcements_CareerCenters_CareerCenterId",
+                        column: x => x.CareerCenterId,
+                        principalTable: "CareerCenters",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Announcements_Faculties_FacultyId",
+                        column: x => x.FacultyId,
+                        principalTable: "Faculties",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Announcements_CareerCenterId",
+                table: "Announcements",
+                column: "CareerCenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Announcements_FacultyId",
+                table: "Announcements",
+                column: "FacultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faculties_CoordinatorId",
+                table: "Faculties",
+                column: "CoordinatorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Announcements");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+        }
+    }
+}
---
+++ Internship.DataAccess/Migrations/20230515220842_AddFacultyToCoordinatorToDb.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internship.DataAccess.Migrations
+{
+    public partial class AddFacultyToCoordinatorToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Announcements_Faculties_FacultyId",
+                table: "Announcements");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Announcements_FacultyId",
+                table: "Announcements");
+
+            migrationBuilder.DropColumn(
+                name: "FacultyId",
+                table: "Announcements");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FacultyId",
+                table: "Announcements",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CoordinatorId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Faculties_Coordinators_CoordinatorId",
+                        column: x => x.CoordinatorId,
+                        principalTable: "Coordinators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Announcements_FacultyId",
+                table: "Announcements",
+                column: "FacultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faculties_CoordinatorId",
+                table: "Faculties",
+                column: "CoordinatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Announcements_Faculties_FacultyId",
+                table: "Announcements",
+                column: "FacultyId",
+                principalTable: "Faculties",
+                principalColumn: "Id");
+        }
+    }
+}
