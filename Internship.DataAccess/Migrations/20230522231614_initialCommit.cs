840dce34dbfc8579a9b55641dbfd14d1b008c43a
Hamza Amin <90584545+Ham252@users.noreply.github.com>
2023-05-23 05:02:21 +0300

    SignalR initial commit
    added hub, service and client side library

---
+++ Internship.DataAccess/Migrations/20230522231614_initialCommit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internship.DataAccess.Migrations
+{
+    public partial class initialCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
