f7f904468ac4e90e51a7ebfa733ebda43aa3a066
Mohammad Soqar <mnsoqar1@gmail.com>
2023-05-29 14:00:30 +0300

    Create submittedApplicationForms.cs

---
+++ Internship.Models/submittedApplicationForms.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.Models
+{
+    public class submittedApplicationForms
+    {
+        public int Id { get; set; }
+
+        [ValidateNever]
+        public string PDFForm { get; set; }
+
+        public int StudentId { get; set; }
+        [ValidateNever]
+        public Student_User Student { get; set; }
+
+        public int InternshipCoordinatorId { get; set; }
+        [ValidateNever]
+        public InternshipCoordinator_User InternshipCoordinator { get; set; }
+    }
+}
