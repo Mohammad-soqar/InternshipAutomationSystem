287799b6953439513f89869f409fa7570f2d6754
Mohammad Soqar <mnsoqar1@gmail.com>
2023-05-29 14:00:00 +0300

    Create HealthInsurance.cs

---
+++ Internship.Models/HealthInsurance.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.Models
+{
+    public class HealthInsurance
+    {
+        public int Id { get; set; }
+
+        [ValidateNever]
+        public string HealthInsurancePDF { get; set; }
+
+        public int StudentId { get; set; }
+        [ValidateNever]
+        public Student_User Student { get; set; }
+
+        public int CareerCenterId { get; set; }
+        [ValidateNever]
+        public CareerCenter_User CareerCenter { get; set; }
+
+    }
+}
