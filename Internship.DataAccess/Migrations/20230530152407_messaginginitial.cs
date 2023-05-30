a74c914795c88e310f3d3b831a3eed913fba9a41
Hamza Amin <90584545+Ham252@users.noreply.github.com>
2023-05-30 21:05:09 +0300

    SignalR Setup + Hubs

---
+++ Internship.DataAccess/Migrations/20230530152407_messaginginitial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internship.DataAccess.Migrations
+{
+    public partial class messaginginitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
