57b05ad8e0b3b2488caf400eb817a25b68b0a733
Mohammad Soqar <mnsoqar1@gmail.com>
2023-06-03 16:06:40 +0300

    Repository

---
+++ Internship.DataAccess/Repository/ReportRepository.cs
@@ -0,0 +1,36 @@
+﻿using Internship.DataAccess.Data;
+using Internship.DataAccess.Repository.IRepository;
+using Internship.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.DataAccess.Repository
+{
+    public class ReportRepository : Repository<Report>, IReportRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ReportRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+
+        }
+
+
+        public void Update(Report obj)
+        {
+            var objFromDb = _db.Reports.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.ReportPdf = obj.ReportPdf;
+  
+               
+
+            }
+        }
+    }
+}
---
+++ Internship.DataAccess/Repository/SaveInternshipRepository.cs
@@ -0,0 +1,37 @@
+﻿using Internship.DataAccess.Data;
+using Internship.DataAccess.Repository.IRepository;
+using Internship.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.DataAccess.Repository
+{
+    public class SaveInternshipRepository : Repository<SavedInternship>, ISaveInternshipRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SaveInternshipRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+
+        }
+
+
+        public void Update(SavedInternship obj)
+        {
+            var objFromDb = _db.SavedInternships.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.InternshipOpportunityId = obj.InternshipOpportunityId;
+                objFromDb.StudentId = obj.StudentId;
+
+
+
+            }
+        }
+    }
+}
