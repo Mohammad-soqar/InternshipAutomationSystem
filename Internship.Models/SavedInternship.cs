82b746440bdc29b3d13c2550c905a3f8fa051117
Mohammad Soqar <mnsoqar1@gmail.com>
2023-06-03 16:06:59 +0300

    Create SavedInternship.cs

---
+++ Internship.Models/SavedInternship.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship.Models
+{
+  
+        public class SavedInternship
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+
+            public int InternshipOpportunityId { get; set; }
+            public InternshipOpportunity InternshipOpportunity { get; set; }
+
+            public int StudentId { get; set; }
+            public Student_User Student { get; set; }
+        }
+    
+}
